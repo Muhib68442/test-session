48026c9888b9d362e9c2210f1877ef9dfaa352e1
Muhibbur Rahman <82113669+Muhib68442@users.noreply.github.com>
2022-05-12 21:22:41 +0600

    cleared

---
+++ insider-ver/prev-ver/prev 3.0.1/about and contact/about-me.js
@@ -0,0 +1,6 @@
+function study(){
+    $(".study").slideToggle(300);
+}
+function address(){
+    $(".address").slideToggle(300);
+}
---
+++ loading/loading.js
@@ -0,0 +1,4 @@
+$(window).on('load',function(){
+    $(".loader").fadeOut(100000);
+    $(".muhib").fadeIn(300000);
+});
