7c8d4aa13a0d9d417836d94243b60872d6c77968
Muhibbur Rahman <82113669+Muhib68442@users.noreply.github.com>
2023-04-30 10:29:14 +0600

    STATIC DEBUGGING

--- MODULES AND BACKUPS/PARALLEL/showcap.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('.content-image');
-const detailsDivs = document.querySelectorAll('.content-text');
-
-images.forEach((img, index) => {
-  img.addEventListener('click', () => {
-    detailsDivs[index].classList.toggle('show');
-  });
-});
